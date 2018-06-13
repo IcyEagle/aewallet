24a8d11dbdabc0d4fcab3d6d5671c67069146abb
gspasov <gvspasov@gmail.com>
2018-06-13 09:57:14 +0300

    [GH-#104] Added formatter to travis check, formatted the code

--- .credo.exs
+++ .credo.exs
@@ -20,7 +20,7 @@
         #
         # You can give explicit globs or simply directories.
         # In the latter case `**/*.{ex,exs}` will be used.
-      #
+        #
         included: ["lib/", "src/", "web/", "apps/"],
         excluded: [~r"/_build/", ~r"/deps/"]
       },
@@ -74,7 +74,6 @@
         #
         {Credo.Check.Design.TagTODO, exit_status: 2},
         {Credo.Check.Design.TagFIXME},
-
         {Credo.Check.Readability.FunctionNames, priority: :higher},
         {Credo.Check.Readability.LargeNumbers, priority: :higher},
         {Credo.Check.Readability.MaxLineLength, priority: :low, max_length: 80},
@@ -92,7 +91,6 @@
         {Credo.Check.Readability.VariableNames, priority: :higher},
         {Credo.Check.Readability.Semicolons},
         {Credo.Check.Readability.SpaceAfterCommas, priority: :higher},
-
         {Credo.Check.Refactor.DoubleBooleanNegation, priority: :higher},
         {Credo.Check.Refactor.CondStatements},
         {Credo.Check.Refactor.CyclomaticComplexity, priority: :high},
@@ -104,7 +102,6 @@
         {Credo.Check.Refactor.Nesting, priority: :high},
         {Credo.Check.Refactor.PipeChainStart},
         {Credo.Check.Refactor.UnlessWithElse},
-
         {Credo.Check.Warning.BoolOperationOnSameValues},
         {Credo.Check.Warning.ExpensiveEmptyEnumCheck},
         {Credo.Check.Warning.IExPry},
@@ -136,7 +133,7 @@
         {Credo.Check.Warning.NameRedeclarationByAssignment, false},
         {Credo.Check.Warning.NameRedeclarationByCase, false},
         {Credo.Check.Warning.NameRedeclarationByDef, false},
-        {Credo.Check.Warning.NameRedeclarationByFn, false},
+        {Credo.Check.Warning.NameRedeclarationByFn, false}
 
         # Custom checks can be created using `mix credo.gen.check`.
         #
